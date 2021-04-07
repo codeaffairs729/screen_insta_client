f3d84cbc36f9687290f538ae9fe7f8baf816d6ba
Reda SELLAK <reda.sllak@gmail.com>
2021-04-07 20:24:06 +0100

    changed video and audio types

--- src/validUploads.js
+++ src/validUploads.js
@@ -1,19 +1,8 @@
 import mimeTypes from "mime-types";
 
-export const validAudioFiles = [
-  "audio/mpeg",
-  "audio/wav",
-  "audio/webm",
-  "audio/ogg",
-];
+export const validAudioFiles = ["audio/mpeg"];
 export const validImageFiles = ["image/png", "image/jpeg", "image/gif"];
-export const validVideoFiles = [
-  "video/mp4",
-  "video/MP2T",
-  "video/3gpp",
-  "video/webm",
-  "video/quicktime",
-];
+export const validVideoFiles = ["video/mp4"];
 
 export const isVideo = (link) => {
   try {
